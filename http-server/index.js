c76219ceb9f082fed5314b4c1d754fe54f02d6a7
achyutp <achyutp10@gmail.com>
2023-06-20 13:36:22 +0545

    stream example

--- http-server/index.js
+++ http-server/index.js
@@ -3,8 +3,11 @@ const http = require('http')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-  fs.readFile('sample.txt', (err, data) => {
-    res.end(data);
-  })
+    const stream = fs.createReadStream('sample.txt');
+    stream.pipe(res)
+  
+//     fs.readFile('sample.txt', (err, data) => {
+//     res.end(data);
+//   })
 })
 server.listen(3000)
